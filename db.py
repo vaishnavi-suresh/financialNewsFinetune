34812173409477299a83546d1d44c71f5507a5d6
Ubuntu <ubuntu@ip-192-168-0-171.us-west-2.compute.internal>
2025-11-08 21:18:25 +0000

    db.py

--- db.py
+++ db.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass, field
+from peft import LoraConfig
 from datasets import load_dataset
 
 ds = load_dataset("boyiwei/newsqa_filtered_sorted", split="train")  # "validation" or "test" also available
