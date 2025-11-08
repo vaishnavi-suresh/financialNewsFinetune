2353332ccb3f2d230de4882c83b42bca059e215d
Ubuntu <ubuntu@ip-192-168-0-205.us-west-2.compute.internal>
2025-11-08 21:00:02 +0000

    new dataset

--- db.py
+++ db.py
@@ -1,5 +1,7 @@
 # db.py
 from datasets import load_dataset
 
-ds = load_dataset("Maluuba/newsqa", split="train")  # "validation" or "test" also available
-print(ds)
+ds = load_dataset("boyiwei/newsqa_filtered_sorted", split="train")  # "validation" or "test" also available
+print(ds[:1])
+
+
