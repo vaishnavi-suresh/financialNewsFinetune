aeb5506a2db044d4450e1df1b601b962e8f533ac
Ubuntu <ubuntu@ip-192-168-0-205.us-west-2.compute.internal>
2025-11-08 21:02:54 +0000

    finetune start

--- db.py
+++ db.py
@@ -1,7 +1,11 @@
-# db.py
 from datasets import load_dataset
 
 ds = load_dataset("boyiwei/newsqa_filtered_sorted", split="train")  # "validation" or "test" also available
 print(ds[:1])
 
+def finetune():
+    ds = ds.shuffle(seed=23)
+    train_dataset = ds.select(range(50000))
+    eval_dataset = sd.select(range(50000, 50500))
 
+    
